5fcfea1117597503168dbe6c19b69031d1ff2a86
joaopugsley <joaopugsleyy@gmail.com>
2024-05-12 14:21:41 -0300

    feat: create page meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+  useSeoMeta({
+    title: "JoãoPugsley | Web Developer",
+    ogTitle: "JoãoPugsley | Web Developer",
+    description: "Explore o portfólio de desenvolvimento web de João Pugsley, que exibe uma ampla variedade de sites criativos e responsivos.",
+    ogDescription: "Explore o portfólio de desenvolvimento web de João Pugsley, que exibe uma ampla variedade de sites criativos e responsivos.",
+    keywords: "desenvolvimento web, web design, portfolio, João Vitor Pugsley, João Pugsley, desenvolvimento front-end, desenvolvimento back-end, design responsivo",
+    viewport: "width=device-width, initial-scale=1",
+    ogType: "website",
+    ogUrl: "https://joaopugsley.dev",
+    ogSiteName: "JoãoPugsley | Web Developer"
+  })
+</script>
+
 <template>
   <Sidebar/>
   <main class="p-2 md:pl-[104px]">
