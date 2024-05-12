5d2eaf7bd4302c438d2a0f383951d7cafcbc9b3a
joaopugsley <joaopugsleyy@gmail.com>
2024-05-12 14:10:14 -0300

    feat: create projects page

---
+++ components/ProjectContainer.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+  const projects = [
+    {
+      image: "/images/projects/howtocs2.png",
+      title: "HOWTOCS2 - Blog",
+      description: "Portal de informações sobre o Counter Strike brasileiro.",
+      live_url: "https://howtocs2.com",
+      source_code: "https://github.com/joaopugsley/howtocs2",
+    },
+    {
+      image: "/images/projects/twitchroulette.png",
+      title: "TwitchRoulette",
+      description: "Realização de sorteios na twitch.tv de maneira fácil, prática e eficaz.",
+      live_url: "https://twitchroulette.live",
+      source_code: "https://github.com/joaopugsley/twitchroulette",
+    },
+    {
+      image: "/images/projects/steam-tools.png",
+      title: "SteamTools",
+      description: "Ferramentas que podem ser úteis na utilização da Steam.",
+      live_url: "https://steam-tools-eta.vercel.app/",
+      source_code: "https://github.com/joaopugsley/steam-tools",
+    },
+    {
+      image: "/images/projects/cs-wrapper.png",
+      title: "CS-WRAPPER",
+      description: "Visualização de inventários de forma simplificada.",
+      source_code: "https://github.com/joaopugsley/cs-wrapper",
+    },
+  ]
+</script>
+
+<template>
+  <ul class="mt-2 grid grid-cols-1 md:grid-cols-2 gap-4 w-full h-full">
+    <Project
+      v-for="project in projects"
+      :image="project.image"
+      :title="project.title"
+      :description="project.description"
+      :live_url="project.live_url"
+      :source_code="project.source_code"
+    />
+  </ul>
+</template>
---
+++ pages/projects.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="w-full flex flex-col p-8 md:p-28 xl:p-36 2xl:p-48">
+    <section class="w-full z-10 flex flex-col space-y-5">
+      <h1 class="text-4xl lg:text-5xl font-bold">Projetos</h1>
+      <ProjectContainer/>
+    </section>
+  </div>
+</template>
