14e60d40d4ece6933c1430e33088ec49fb23a277
joaopugsley <joaopugsleyy@gmail.com>
2024-05-12 13:00:57 -0300

    feat: create footer component

---
+++ components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <footer class="w-full min-h-8 sm:min-h-16 bg-zinc-950 border-t border-zinc-800 p-4 z-30 flex flex-col items-center">
+    <span class="text-sm text-zinc-400">Este site foi desenvolvido em Vue.js com Nuxt 3 e TypeScript</span>
+    <span class="text-base">&copy; {{ new Date().getFullYear() }} João Vitor Pugsley. Todos os direitos reservados.</span>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,6 @@
   <main class="p-2 md:pl-[104px]">
     <slot></slot>
   </main>
+  <Footer/>
   <Particles class="-z-10"/>
 </template>
