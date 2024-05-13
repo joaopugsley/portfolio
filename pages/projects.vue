c260d320fc517a2ad14d97b3a112d9df103321fc
joaopugsley <joaopugsleyy@gmail.com>
2024-05-13 03:52:24 -0300

    feat: create interaction tip on desktop

--- pages/projects.vue
+++ pages/projects.vue
@@ -2,6 +2,9 @@
   <div class="w-full flex flex-col p-8 md:p-28 xl:p-36 2xl:p-48">
     <section class="w-full z-10 flex flex-col space-y-5">
       <h1 class="text-4xl lg:text-5xl font-bold">Projetos</h1>
+      <div class="hidden md:flex w-full p-4 bg-neutral-400/10 border border-neutral-400 rounded-lg text-neutral-300">
+        <p>💡 Passe o mouse por cima do projeto para ver mais opções de interação.</p>
+      </div>
       <ProjectContainer/>
     </section>
   </div>
